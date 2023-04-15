a4f90df57c10b72c27fc235ed0962951ceb20cce
Ayodeji Yekeen <ayodejiyou@gmail.com>
2023-04-15 01:44:52 +0000

    add file or image upload

--- config/urls.py
+++ config/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -9,4 +11,4 @@ urlpatterns = [
     # Local apps
     path("", include("pages.urls")),
     path("books/", include("books.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
